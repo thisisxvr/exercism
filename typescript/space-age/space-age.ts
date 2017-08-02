b37b4fc11ac252ba68493846822f129a4ed53c35
Xavier Francis <thisisxvr@gmail.com>
2017-08-02 22:14:53 +1200

    [ts] space-age iteration 2.

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,46 +1,15 @@
-const enum OrbitalPeriod {
-  Mercury = 0.2408467,
-  Venus = 0.61519726,
-  Mars = 1.8808158,
-  Jupiter = 11.862615,
-  Saturn = 29.447498,
-  Uranus = 84.016846,
-  Neptune = 164.79132
-}
-
 export default class SpaceAge {
   constructor(private readonly _seconds: number) { }
   get seconds(): number { return this._seconds }
 
-  public onEarth(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25).toFixed(2))
-  }
+  private solarYear = (period: number): number => { return parseFloat((this._seconds / (31557600 * period)).toFixed(2)) }
 
-  public onMercury(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25 / OrbitalPeriod.Mercury).toFixed(2))
-  }
-
-  public onVenus(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25 / OrbitalPeriod.Venus).toFixed(2))
-  }
-
-  public onMars(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25 / OrbitalPeriod.Mars).toFixed(2))
-  }
-
-  public onJupiter(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25 / OrbitalPeriod.Jupiter).toFixed(2))
-  }
-
-  public onSaturn(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25 / OrbitalPeriod.Saturn).toFixed(2))
-  }
-
-  public onUranus(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25 / OrbitalPeriod.Uranus).toFixed(2))
-  }
-
-  public onNeptune(): number {
-    return Number((this._seconds / 60 / 60 / 24 / 365.25 / OrbitalPeriod.Neptune).toFixed(2))
-  }
+  public onEarth   = (): number => this.solarYear(1)
+  public onMercury = (): number => this.solarYear(0.2408467)
+  public onVenus   = (): number => this.solarYear(0.61519726)
+  public onMars    = (): number => this.solarYear(1.8808158)
+  public onJupiter = (): number => this.solarYear(11.862615)
+  public onSaturn  = (): number => this.solarYear(29.447498)
+  public onUranus  = (): number => this.solarYear(84.016846)
+  public onNeptune = (): number => this.solarYear(164.79132)
 }
